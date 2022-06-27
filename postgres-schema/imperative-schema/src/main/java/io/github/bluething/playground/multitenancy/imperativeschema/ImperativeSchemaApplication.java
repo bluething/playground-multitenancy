808c4a7fdac081206dd2abbf4ca825b0346b37d4
Habib Ali Machpud <habib.machpud@gmail.com>
2022-06-27 23:07:38 +0700

    Init

---
+++ postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/ImperativeSchemaApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.multitenancy.imperativeschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperativeSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperativeSchemaApplication.class, args);
+	}
+
+}
---
+++ postgres-schema/imperative-schema/src/test/java/io/github/bluething/playground/multitenancy/imperativeschema/ImperativeSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.multitenancy.imperativeschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperativeSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
