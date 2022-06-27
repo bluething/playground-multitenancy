c5c4e95cc7bd0fe47872ced7107d4d004014276f
Habib Ali Machpud <habib.machpud@gmail.com>
2022-06-27 16:23:34 +0700

    Init project

---
+++ postgres-rls/imperative/src/test/java/io/github/bluething/playground/multitenancy/imperative/ImperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.multitenancy.imperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
