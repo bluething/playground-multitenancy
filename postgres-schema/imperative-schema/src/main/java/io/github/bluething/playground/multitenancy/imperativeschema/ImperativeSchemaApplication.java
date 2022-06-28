85bdec6a0eb18e64eef2a9ed92d8b54034b2e749
Habib Ali Machpud <habib.machpud@gmail.com>
2022-06-28 16:08:47 +0700

    Add properties and multitenancy package

--- postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/ImperativeSchemaApplication.java
+++ postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/ImperativeSchemaApplication.java
@@ -2,8 +2,10 @@ package io.github.bluething.playground.multitenancy.imperativeschema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ImperativeSchemaApplication {
 
 	public static void main(String[] args) {
