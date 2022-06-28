dfc42450df42fb685dd7b19fe09f84dac1c52a05
Habib Ali Machpud <habib.machpud@gmail.com>
2022-06-28 15:12:17 +0700

    Update DepartmentPersistenceAdapter.java
    Persistence adapter

--- postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/infrastucture/persistence/DepartmentPersistenceAdapter.java
+++ postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/infrastucture/persistence/DepartmentPersistenceAdapter.java
@@ -1,15 +1,35 @@
 package io.github.bluething.playground.multitenancy.imperativeschema.infrastucture.persistence;
 
 import io.github.bluething.playground.multitenancy.imperativeschema.domain.Department;
+import io.github.bluething.playground.multitenancy.imperativeschema.domain.Employee;
 import io.github.bluething.playground.multitenancy.imperativeschema.domain.department.DepartmentPersistencePort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class DepartmentPersistenceAdapter implements DepartmentPersistencePort {
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    public DepartmentPersistenceAdapter(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
     @Override
     public List<Department> getDepartments() {
-        return null;
+        Iterable<DepartmentEntity> departmentEntities = departmentRepository.findAll();
+        List<Department> departments = new ArrayList<>();
+        List<Employee> employees = null;
+        for (DepartmentEntity departmentEntity : departmentEntities) {
+            employees = new ArrayList<>();
+            for (EmployeeEntity employeeEntity : departmentEntity.getEmployees()) {
+                employees.add(new Employee(employeeEntity.getEmployeeId(), employeeEntity.getName()));
+            }
+            departments.add(new Department(departmentEntity.getDepartmentId(), departmentEntity.getName(), employees));
+        }
+        return departments;
     }
 }
