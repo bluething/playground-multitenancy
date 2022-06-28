d52d7eda0dd123be3263be641a92625967aa9db6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-06-28 15:12:43 +0700

    Update EmployerController.java
    Prepare the response

--- postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/infrastucture/rest/EmployerController.java
+++ postgres-schema/imperative-schema/src/main/java/io/github/bluething/playground/multitenancy/imperativeschema/infrastucture/rest/EmployerController.java
@@ -1,11 +1,15 @@
 package io.github.bluething.playground.multitenancy.imperativeschema.infrastucture.rest;
 
+import io.github.bluething.playground.multitenancy.imperativeschema.domain.Department;
+import io.github.bluething.playground.multitenancy.imperativeschema.domain.Employee;
 import io.github.bluething.playground.multitenancy.imperativeschema.domain.department.DepartmentRestPort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +23,16 @@ public class EmployerController {
 
     @GetMapping(path = "/departments")
     public ResponseEntity<List<DepartmentPayload>> getDepartments() {
-        return null;
+        List<Department> departments = departmentRestPort.getDepartments();
+        List<DepartmentPayload> departmentPayloads = new ArrayList<>();
+        List<EmployeePayload> employeePayloads = null;
+        for (Department department : departments) {
+            employeePayloads = new ArrayList<>();
+            for (Employee employee : department.employees()) {
+                employeePayloads.add(new EmployeePayload(employee.employeeId(), employee.name()));
+            }
+            departmentPayloads.add(new DepartmentPayload(department.departmentId(), department.name(), employeePayloads));
+        }
+        return new ResponseEntity<>(departmentPayloads, HttpStatus.OK);
     }
 }
